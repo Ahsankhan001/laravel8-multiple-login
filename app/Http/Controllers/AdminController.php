a556913c79cbc8cef2a5d192372b0c7527944691
Ahsan Ahmad <Ahsan.Ahmad@SRMTECHSOL.Local>
2021-10-13 18:01:52 +0530

    add new files

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator, Auth;
+
+class AdminController extends Controller
+{
+    public function authenticate(Request $request){
+        $this->validate($request,[
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+         if(Auth::guard('admin')->attempt(['email'=>$request->email, 'password'=>$request->password], $request->get('remember')))
+{       return redirect()->route('admin.dashboard');
+
+}   else{
+     session()->flash('error', 'Email/password in crrect');
+     return back()->withoutInput($request->only('email'));
+}     
+
+    }
+
+    public function logout(){
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login');
+    }
+    
+   
+}
